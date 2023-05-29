7b7a554302e2765c08de3370fd4ccf11d5198446
pellse <sebastien.pelletier@sekmed.ca>
2023-05-29 19:25:20 -0400

    Adding `@RestartScope` to test

--- src/test/java/io/github/pellse/example/PatientMonitoringApplicationTest.java
+++ src/test/java/io/github/pellse/example/PatientMonitoringApplicationTest.java
@@ -1,6 +1,7 @@
 package io.github.pellse.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -10,28 +11,8 @@ import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.testcontainers.utility.DockerImageName.parse;
 
-@TestConfiguration
+@TestConfiguration(proxyBeanMethods = false)
 public class PatientMonitoringApplicationTest {
-
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgreSQLContainer() {
-        return new PostgreSQLContainer<>("postgres:15.1-alpine");
-    }
-
-    @Bean
-    @ServiceConnection
-    MongoDBContainer mongoDBContainer() {
-        return new MongoDBContainer("mongo:5.0");
-    }
-
-    @Bean
-    @ServiceConnection
-    public KafkaContainer kafka() {
-        return new KafkaContainer(parse("confluentinc/cp-kafka:7.4.0"))
-                .withKraft()
-                .withReuse(true);
-    }
 
     public static void main(String[] args) {
 
@@ -39,5 +20,26 @@ public class PatientMonitoringApplicationTest {
                 .from(PatientMonitoringApplication::main)
                 .with(PatientMonitoringApplicationTest.class)
                 .run(args);
+    }
+
+    @Bean
+    @ServiceConnection
+    @RestartScope
+    PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>("postgres:15.1-alpine");
+    }
+
+    @Bean
+    @ServiceConnection
+    @RestartScope
+    MongoDBContainer mongoDBContainer() {
+        return new MongoDBContainer("mongo:5.0");
+    }
+
+    @Bean
+    @ServiceConnection
+    @RestartScope
+    public KafkaContainer kafka() {
+        return new KafkaContainer(parse("confluentinc/cp-kafka:7.4.0"));
     }
 }
