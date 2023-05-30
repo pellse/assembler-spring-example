a2ef23a6591bb5bb24860948f9a6458a203a78a7
pellse <sebastien.pelletier@sekmed.ca>
2023-05-30 18:16:31 -0400

    Renamed `percentage` to `spO2Value` in `SpO2` Record

--- src/main/java/io/github/pellse/example/domain/spo2/SpO2.java
+++ src/main/java/io/github/pellse/example/domain/spo2/SpO2.java
@@ -4,5 +4,5 @@ import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 
-public record SpO2(@Id Integer id, Integer patientId, int percentage, LocalDateTime time) {
+public record SpO2(@Id Integer id, Integer patientId, int spO2Value, LocalDateTime time) {
 }
