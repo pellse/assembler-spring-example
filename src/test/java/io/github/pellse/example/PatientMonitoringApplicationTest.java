d6c0643efcfd5da3e7d425bda0a66407edc11ab6
pellse <sebastien.pelletier@sekmed.ca>
2023-05-30 00:10:02 -0400

    Fix @ServiceConnection not working with `KafkaContainer`, use `DynamicPropertyRegistry` to set "spring.kafka.properties.bootstrap.servers" property

--- src/test/java/io/github/pellse/example/PatientMonitoringApplicationTest.java
+++ src/test/java/io/github/pellse/example/PatientMonitoringApplicationTest.java
@@ -1,10 +1,10 @@
 package io.github.pellse.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -24,22 +24,20 @@ public class PatientMonitoringApplicationTest {
 
     @Bean
     @ServiceConnection
-    @RestartScope
     PostgreSQLContainer<?> postgreSQLContainer() {
         return new PostgreSQLContainer<>("postgres:15.1-alpine");
     }
 
     @Bean
     @ServiceConnection
-    @RestartScope
     MongoDBContainer mongoDBContainer() {
         return new MongoDBContainer("mongo:5.0");
     }
 
     @Bean
-    @ServiceConnection
-    @RestartScope
-    public KafkaContainer kafka() {
-        return new KafkaContainer(parse("confluentinc/cp-kafka:7.4.0"));
+    public KafkaContainer kafka(DynamicPropertyRegistry registry) {
+        var kafka = new KafkaContainer(parse("confluentinc/cp-kafka:7.4.0"));
+        registry.add("spring.kafka.properties.bootstrap.servers", () -> kafka.getHost() + ":" + kafka.getFirstMappedPort());
+        return kafka;
     }
 }
