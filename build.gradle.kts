f9d2df0ef45f3002658e15497508dc5d7b1f12fe
pellse <sebastien.pelletier@sekmed.ca>
2024-04-25 21:16:41 -0400

    Support for Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "io.github.pellse"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenLocal()
