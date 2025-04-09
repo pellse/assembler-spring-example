2e21e746901281d727f614ee194132d9683eec80
pellse <sebastien.pelletier@sekmed.ca>
2025-04-09 14:24:08 -0400

    Adding error handling to `DiagnosisAIService`

--- src/main/java/io/github/pellse/patientobservation/common/Diagnosis.java
+++ src/main/java/io/github/pellse/patientobservation/common/Diagnosis.java
@@ -1,4 +1,12 @@
 package io.github.pellse.patientobservation.common;
 
 public record Diagnosis(Integer patientId, String diagnosis) {
+
+    public Diagnosis(Vitals vitals) {
+        this(vitals.patient().id());
+    }
+
+    public Diagnosis(Integer patientId) {
+        this(patientId, "Unavailable");
+    }
 }
--- src/main/java/io/github/pellse/patientobservation/service/diagnosis/DiagnosisAIService.java
+++ src/main/java/io/github/pellse/patientobservation/service/diagnosis/DiagnosisAIService.java
@@ -3,6 +3,7 @@ package io.github.pellse.patientobservation.service.diagnosis;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.pellse.patientobservation.common.Diagnosis;
 import io.github.pellse.patientobservation.common.Vitals;
+import org.slf4j.Logger;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.core.ParameterizedTypeReference;
@@ -10,14 +11,20 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import static io.github.pellse.util.CheckedFunction.unchecked;
 import static java.util.function.Function.identity;
+import static org.slf4j.LoggerFactory.getLogger;
+import static reactor.core.publisher.Flux.fromIterable;
 import static reactor.core.publisher.Mono.just;
 import static reactor.core.scheduler.Schedulers.boundedElastic;
 
 @Service
 public class DiagnosisAIService {
+
+    private static final Logger logger = getLogger(DiagnosisAIService.class);
 
     private static final String promptTemplate = """
             You are given an array of JSON objects, each representing a patient's vitals.
@@ -52,10 +59,15 @@ public class DiagnosisAIService {
 
     public Flux<Diagnosis> getDiagnosesFromLLM(List<Vitals> vitalsList) {
 
+        Consumer<Throwable> logError = e -> logger.error(e.toString());
+        Function<Throwable, Flux<Diagnosis>> defaultDiagnoses = e -> fromIterable(vitalsList).map(Diagnosis::new);
+
         return just(vitalsList)
                 .map(unchecked(objectMapper::writeValueAsString))
                 .mapNotNull(this::getDiagnosesFromLLM)
                 .flatMapIterable(identity())
+                .doOnError(logError)
+                .onErrorResume(defaultDiagnoses)
                 .subscribeOn(boundedElastic());
     }
 
