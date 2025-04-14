d450ba73fe20d785edab0819891edc632640eeb9
pellse <sebastien.pelletier@sekmed.ca>
2025-04-14 11:16:40 -0400

    Only retry when emitting on the sink results in FAIL_NON_SERIALIZED

--- src/main/java/io/github/pellse/util/reactive/ReactiveBridge.java
+++ src/main/java/io/github/pellse/util/reactive/ReactiveBridge.java
@@ -5,6 +5,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 import reactor.core.publisher.Sinks.EmissionException;
+import reactor.core.publisher.Sinks.EmitResult;
 import reactor.kafka.receiver.ReceiverOffset;
 import reactor.util.retry.Retry;
 import reactor.util.retry.RetryBackoffSpec;
@@ -16,6 +17,7 @@ import static java.util.Objects.requireNonNull;
 import static org.springframework.kafka.support.KafkaHeaders.ACKNOWLEDGMENT;
 import static reactor.core.publisher.Mono.error;
 import static reactor.core.publisher.Mono.just;
+import static reactor.core.publisher.Sinks.EmitResult.*;
 import static reactor.core.publisher.Sinks.EmitResult.FAIL_CANCELLED;
 
 public class ReactiveBridge {
@@ -45,12 +47,21 @@ public class ReactiveBridge {
                 just(message)
                         .map(Message::getPayload)
                         .map(sink::tryEmitNext)
-                        .flatMap(emitResult -> emitResult.isSuccess() ? just(emitResult) : error(new EmissionException(emitResult)))
+                        .flatMap(emitResult -> shouldRetry(emitResult) ? error(new EmissionException(emitResult)) : just(emitResult))
                         .doOnNext(emitResult -> requireNonNull(message.getHeaders().get(ACKNOWLEDGMENT, ReceiverOffset.class)).acknowledge())
                         .transform(mono -> retry != null ? mono.retryWhen(retry) : mono)
                         .transform(mono -> onError != null ? mono.doOnError(onError) : mono)
                         .onErrorResume(e -> just(FAIL_CANCELLED)) // What we return here doesn't matter, but it's important to return a value so that the Reactive Kafka Binder doesn't stop listening to the Kafka topic
         ).then(); // Converts our Flux to Mono<Void>, which will complete when the Flux completes. The Reactive Kafka Binder will subscribe to this Mono<Void> so we don't have to subscribe to it manually.
+    }
+
+    /**
+     * Checks if the {@code emitResult} indicates a failure that should be retried.
+     * We consider a failure to be retryable only if it is a serialization failure, meaning 2 or more threads
+     * are trying to emit to the same sink at the same time.
+     */
+    private boolean shouldRetry(EmitResult emitResult) {
+        return emitResult == FAIL_NON_SERIALIZED;
     }
 
     /**
